558a6e634cbab7d0b80ce9f00e48f0bef0627a86
Brian <42351210+jassiel2911@users.noreply.github.com>
2019-03-17 21:15:21 -0600

    Create sumamat.c

---
+++ sumamat.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "sumadematrices.h"
+
+int main (void){
+	int mat1[3][3]={}, mat2[3][3]={};
+	CapturarM();
+	void sumar(int a[3][3],int b[3][3]);
+	Resultado();	
+	return 0;
+}
